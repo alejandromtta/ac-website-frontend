94cc6e32ca4efc4ecd51bdab658b44d365adddc6
alejandromotta <alejandromotta808@gmail.com>
2024-01-03 18:34:26 -0500

    added product and services

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -19,15 +19,22 @@ const Home = () => {
           </div>
           <div className="text-lg text-left text-white mt-11">
             <p>
-            AC ALTURAS Y CONFINADOS, los recibe con las manos abiertas, estamos para acompañarte y brindarte mayor asesoría en el proceso de capacitación para trabajo seguro en alturas.{" "}
+              AC ALTURAS Y CONFINADOS, los recibe con las manos abiertas,
+              estamos para acompañarte y brindarte mayor asesoría en el proceso
+              de capacitación para trabajo seguro en alturas.{" "}
             </p>
           </div>
           <div className="justify-start homeFirstViewButton">
-          <a
-          rel="noreferrer"
-          target="_blank"
-          href="https://maps.app.goo.gl/9YFvBcUezQzUiYgaA"
-        > <button className="flex flex-col justify-center m-auto mt-3 text-center align-middle">Inscribete</button> </a>
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://maps.app.goo.gl/9YFvBcUezQzUiYgaA"
+            >
+              {" "}
+              <button className="flex flex-col justify-center m-auto mt-3 text-center align-middle">
+                Inscribete
+              </button>{" "}
+            </a>
           </div>
         </div>
       </div>
@@ -35,7 +42,10 @@ const Home = () => {
         <p className="thirdView__title">Bienvenido!</p>
         <div className="thirdView__descriptionContainer">
           <p className="thirdView__description">
-          Somos una entidad especializada en formación y entrenamiento en protección contra caídas para trabajo seguro en alturas, dedicados a todas actividades relacionadas con el Sistema de Gestión de la Seguridad y Salud en el Trabajo.
+            Somos una entidad especializada en formación y entrenamiento en
+            protección contra caídas para trabajo seguro en alturas, dedicados a
+            todas actividades relacionadas con el Sistema de Gestión de la
+            Seguridad y Salud en el Trabajo.
           </p>
         </div>
       </div>
@@ -47,12 +57,12 @@ const Home = () => {
         <div className="flex flex-col justify-center mt-40 ml-5 space-y-10 align-middle md:space-y-0 md:mr-0 md:space-x-10 md:flex-row">
           {cardsInformation.map(
             ({ icon, title, description, id }: IHomeCard) => (
-              <Link to="/products"> <HomeCard
+              <HomeCard
                 icon={icon}
                 title={title}
                 description={description}
                 id={id}
-              /> </Link>
+              />
             )
           )}
         </div>
